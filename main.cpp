49722d87b3bda60096a000f6bcb90b7e7ad20d8b
Amongus7719 <vlad4lifeman@yandex.ru>
2022-08-09 01:30:05 +0300

    new file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
-int main()
-{
-	std::cout<<"Hello World"<<std::endl;
-	std::cout<<"Second string"<<std::endl;
-	return 0
+using namespace std;
+
+int main(){
+	cout<<"hello world"<<endl;
+	return 0;
 }
