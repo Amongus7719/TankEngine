3fb0e9dfb85d3f9516a7f2515391b68ca3f262a8
Amongus7719 <vlad4lifeman@yandex.ru>
2022-08-09 07:03:05 +0300

    Signed-off-by: Amongus7719 <vlad4lifeman@yandex.ru>

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-	cout<<"hello world"<<endl;
-	system("pause");
-	return 0;
-}
+-#include<iostream>
+-using namespace std;
+-
+-int main(){
+-	cout<<"hello world"<<endl;
+-	system("pause");
+-	return 0;
+-}
